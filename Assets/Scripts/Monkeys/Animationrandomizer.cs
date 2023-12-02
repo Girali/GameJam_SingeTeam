a74dd9bfda71c4d51749191f0e04cb35768ab037
JameFLC <jameflc.contact@gmail.com>
2023-12-02 22:21:37 +0100

    Added random seated animations for monkeys

---
+++ Assets/Scripts/Monkeys/Animationrandomizer.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using UnityEngine;
+
+
+
+namespace Monkeys
+{
+    public class Animationrandomizer : MonoBehaviour
+    {
+        [SerializeField]
+        private float MinDelay = 0;
+        [SerializeField]
+        private float MaxDelay = 0;
+
+        [SerializeField] private string RandomTrigger ="Action";
+        [SerializeField] private string RandomIndexName = "ActionIndex";
+        [SerializeField] private int MaxIndex = 6;
+    
+        // Members
+        private Animator anim;
+    
+        // Start is called before the first frame update
+        void Start()
+        {
+            anim = GetComponent<Animator>();
+
+            if (anim is null)
+            {
+                return;
+            }
+
+            StartCoroutine(Randomise());
+        }
+
+        IEnumerator Randomise()
+        {
+            while (true)
+            {
+                yield return new WaitForSeconds(Mathf.Max(0, Random.Range(MinDelay, MaxDelay)));
+                
+                anim.SetInteger(RandomIndexName ,Random.Range(0,MaxIndex + 1));
+                anim.SetTrigger(RandomTrigger);
+                
+            }
+        }
+    }
+}
