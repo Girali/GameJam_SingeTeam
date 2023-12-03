425d553d3f11a73cc7064d6725f1f19c0359e355
GabrielSalach <gabsalach@orange.fr>
2023-12-03 22:00:48 +0100

    Big map loot points + arrow pointing towards victory

--- Assets/ArrowGUI.cs
+++ Assets/ArrowGUI.cs
@@ -7,14 +7,20 @@ using UnityEngine;
 public class ArrowGUI : MonoBehaviour
 {
     private Lootable[] points;
+    private VictoryScreen victoryTrigger;
     private void Awake()
     {
         points = FindObjectsByType<Lootable>(FindObjectsSortMode.None);
+        victoryTrigger = FindObjectOfType<VictoryScreen>();
     }
 
     private void Update()
     {
         Lootable[] ltbs = points.Where(e => !e.Looted).OrderBy(e => Vector3.Distance(transform.position, e.transform.position)).ToArray();
+        if (ltbs.Length == 0)
+        {
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(transform.position - victoryTrigger.transform.position),  0.01f );
+        }
         
         transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(transform.position - ltbs[0].transform.position),  0.01f );
     }
